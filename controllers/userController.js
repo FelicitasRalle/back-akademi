b98f29a8bfc805f673c53d4c98254a729c6dab71
Felicitas Ralle <felicitasralle@gmail.com>
2025-05-05 17:14:15 -0300

    se hizo el controller del usuario

---
+++ controllers/userController.js
@@ -0,0 +1,60 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+//registro nuevo usuario
+const registerUser = async (req, res) => {
+    const{ nombre, email, password, rol} = req.body;
+
+    try{
+        //verfico si el usuario ya existe
+        const existingUser = await User.findOne({ email });
+        if(existingUser) return res.status(400).json({mensaje: 'El usuario ya existe en el servidor'});
+
+        //hasheo la contraseña
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        //crear nuevo usuario
+        const newUser = new User({
+            nombre,
+            email,
+            password: hashedPassword,
+            rol
+        });
+
+        await newUser.save();
+
+        res.status(201).json({ mensjae: 'El usuario se registro correctamente'});
+    }catch (error){
+        res.status(500).json({ mensaje: 'Se produjo un error al registrar el usuario'});
+    }
+};
+
+//login
+const loginUser = async (req, res) =>{
+    const { email, password} = req.body;
+
+    try{
+        const user = await User.findOne({ email});
+        if (!user) return res.status(400).json({ mensaje: 'Las credenciales son invalidas'});
+
+        const validPassword = await bcrypt.compare(password, user.password);
+        if(!validPassword) return res.status(400).json({ mensaje: 'Las credenciales son invalidas'});
+
+        //generar jwt
+        const token = jwt.sign(
+            { id: user._id, rol: user.rol},
+            process.env.JWT_SECRET
+        );
+
+        res.json({ mensaje: 'Login existoso', token});
+    }catch(error){
+        res.status(500).json({ mensaje: 'Error al iniciar sesion'});
+    }
+};
+
+module.exports = {
+    registerUser,
+    loginUser
+};
