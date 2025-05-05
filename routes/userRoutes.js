f10b03d3f81119c8830e25874f148edf42d2a972
Felicitas Ralle <felicitasralle@gmail.com>
2025-05-05 17:24:03 -0300

    se modifico el archivo userRoutes para que funcione correctamente

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,17 @@
 //ruta del usuario
 const express = require('express');
 const router = express.Router();
+const { registerUser, loginUser} = require('../controllers/userController');
 
-//luego voy a importar los controladores a medida q los cree
-
+//ruta de prueba
 router.get('/', (req,  res)=>{
-    res.send('la ruta de usuarios esta funcionando');
+    res.json({ mensaje: 'la ruta de usuarios esta funcionando correctamente'});
 });
+
+//registro
+router.post('/register', registerUser);
+
+//login
+router.post('/login', loginUser);
 
 module.exports = router;
