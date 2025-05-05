4b43a0176f5587c7b24581ccce60b1846de3befe
Felicitas Ralle <felicitasralle@gmail.com>
2025-05-05 19:55:36 -0300

    se modifico el user routes para agregar las rutas del abm

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,22 @@
 //ruta del usuario
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser} = require('../controllers/userController');
+const {
+    registerUser,
+    loginUser,
+    getUsers,
+    updateUser,
+    deleteUser
+} = require('../controllers/userController');
 
-//ruta de prueba
-router.get("/", (req,  res)=>{
-    res.json({ mensaje: 'la ruta de usuarios esta funcionando correctamente'});
-});
+const { protect, adminOnly } = require('../middlewares/authMiddleware');
 
-//registro
-router.post('/register', registerUser);
-
-//login
+router.post('/register', protect, adminOnly, registerUser);
 router.post('/login', loginUser);
+
+//endpoints del ABM
+router.get('/', protect, adminOnly, getUsers); //listar usuarios
+router.put('/:id', protect, adminOnly, updateUser); //editar usuarios
+router.delete('/:id', protect, adminOnly, deleteUser); // baja de usuario
 
 module.exports = router;
