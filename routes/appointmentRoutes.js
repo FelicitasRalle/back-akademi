e29e15dc64bdc15c26f5505bc7d1ee753a977a7a
Felicitas Ralle <felicitasralle@gmail.com>
2025-05-07 03:29:40 -0300

    se crearon las rutas para los turnos

---
+++ routes/appointmentRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router;
+
+const{
+    createAppointment,
+    getAppointments,
+    updateAppointment,
+    deleteAppointment
+} = require('../controllers/appointmentController');
+
+const { protect } = require('../middlewares/authMiddleware');
+const checkRole = require('../middlewares/checkRole');
+
+router.get('/', protect, checkRole('admin', 'recepcionista'), getAppointments);
+router.post('/', protect, checkRole('admin', 'recepcionista'), createAppointment);
+router.put('/:id', protect, checkRole('admin', 'recepcionista'), updateAppointment);
+router.delete('/:id', protect, checkRole('admin', 'recepcionista'), deleteAppointment);
+
+module.exports = router;
